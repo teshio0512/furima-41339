361930557ec401badfb913c4899504dec2d04413
teshio0512 <teshio0512@gmail.com>
2024-11-04 15:42:12 +0900

    itemsコントローラーに必要なアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :state_id, :fee_id, :region_id, :day_id, :price,
+                                 :image).merge(user_id: current_user.id)
+  end
 end
