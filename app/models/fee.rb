3496493f2842af3464e2f53334ec9c6dfb0ffecb
teshio0512 <teshio0512@gmail.com>
2024-11-04 15:41:48 +0900

    ActiveHashで使用するモデルの作成

---
+++ app/models/fee.rb
@@ -0,0 +1,10 @@
+class Fee < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '着払い(購入者負担)' },
+    { id: 3, name: }
+  ]
+
+  include ActiveHash::Associations
+  has_many :items
+end
---
+++ app/models/item.rb
@@ -0,0 +1,20 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :fee
+  belongs_to :region
+  belongs_to :day
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, numericality: { in: 300..9_999_999 }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :state_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :region_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true, blob: { content_type: :image }
+end
