29a21aad4c5d67ee8949df0cdd1b36fc566fa18a
teshio0512 <teshio0512@gmail.com>
2024-11-07 21:25:59 +0900

    itemモデル単体テストのための準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    name            { Faker::Japanese::Name.name }
+    explanation     { Faker::Lorem.paragraph }
+    category_id     { rand(2..11) }
+    state_id        { rand(2..7) }
+    fee_id          { rand(2..3) }
+    region_id       { rand(2..48) }
+    day_id          { rand(2..4) }
+    price           { rand(300..9_999_999) }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/天使の画像.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
