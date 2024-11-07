a8e1a0e9b23b6da243858d190d5e93af62bc4e24
teshio0512 <teshio0512@gmail.com>
2024-11-07 21:24:42 +0900

    Math.floorの追加とrenderイベントによるイベント発火を追加

--- app/javascript/items/new.js
+++ app/javascript/items/new.js
@@ -2,8 +2,8 @@ function calculator() {
   const priceForm = document.getElementById("item-price");
   priceForm.addEventListener('input', function(){
     const inputPrice = document.getElementById("item-price").value;
-    const salesCommission = inputPrice * 0.1;
-    const salesProfit = inputPrice - salesCommission;
+    const salesCommission = Math.floor(inputPrice * 0.1);
+    const salesProfit = Math.floor(inputPrice - salesCommission);
     const charCommission = document.getElementById("add-tax-price");
     charCommission.innerHTML = `${salesCommission}`;
     const charProfit = document.getElementById("profit")
@@ -12,3 +12,4 @@ function calculator() {
 }
 
 window.addEventListener('turbo:load', calculator);
+window.addEventListener('turbo:render', calculator);
