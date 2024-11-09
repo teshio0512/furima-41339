6b98ee352132b067e322d663fd23278e16c454c3
teshio0512 <teshio0512@gmail.com>
2024-11-09 20:33:46 +0900

    エラーメッセージはデフォルトに修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,8 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :explanation, presence: true
-  with_options presence: true, numericality: { in: 300..9_999_999, message: 'は300〜9999999の範囲内で入力してください' },
-               format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' } do
+  with_options presence: true, numericality: { in: 300..9_999_999 },
+               format: { with: /\A[0-9]+\z/ } do
     validates :price
   end
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
