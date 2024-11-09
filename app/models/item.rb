94a0d7948a61a68bafecc36c4a122c7df4bd328f
teshio0512 <teshio0512@gmail.com>
2024-11-09 18:29:07 +0900

    priceのバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,10 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :price, numericality: { in: 300..9_999_999 }
+  with_options presence: true, numericality: { in: 300..9_999_999, message: 'は300〜9999999の範囲内で入力してください' },
+               format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' } do
+    validates :price
+  end
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :state_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :fee_id, numericality: { other_than: 1, message: "can't be blank" }
