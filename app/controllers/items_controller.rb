36627125c78efe60bb5b4b4eab0b8f79db56538e
teshio0512 <teshio0512@gmail.com>
2024-11-12 20:15:31 +0900

    set_itemの呼び出し順変更によるリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -42,8 +42,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    item = Item.find(params[:id])
-    return if user_signed_in? && item.user.id == current_user.id
+    return if user_signed_in? && @item.user.id == current_user.id
 
     redirect_to action: :index
   end
