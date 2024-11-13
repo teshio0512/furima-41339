ef905ac9ce2f34179bc32793a5683049a3f672fa
teshio0512 <teshio0512@gmail.com>
2024-11-13 22:21:21 +0900

    未ログイン時のページ遷移制限追加とリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order('created_at DESC')
@@ -35,8 +35,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    return unless user_signed_in? && @item.user_id == current_user.id
-
     @item.destroy
     redirect_to root_path
   end
