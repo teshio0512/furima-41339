349dda06275c03779eb328b8032874dd865977d5
teshio0512 <teshio0512@gmail.com>
2024-11-24 22:22:00 +0900

    purchasesコントローラーにFormオブジェクトを用いた処理を記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,27 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
+    @purchase_address = PurchaseAddress.new
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:post_code, :region_id, :city, :street_address, :building_name, :phone_number).merge(
+      item_id: params[:item_id], user_id: current_user.id
+    )
   end
 end
